5b5bf2e7a38ca84a71be8b85d9244eebdf5cccbb
juliax28 <juliax28@outlook.com>
2024-09-10 19:12:55 -0400

    Made the record

--- topics/instructions/Drawing/js/script.js
+++ topics/instructions/Drawing/js/script.js
@@ -12,6 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
+    createCanvas(640, 640)
 
 }
 
@@ -20,5 +21,18 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(150, 150, 150)
+
+    push();
+    fill(255, 0, 0);
+    stroke(255);
+    ellipse(320, 320, 480, 480);
+    pop();
+
+    push();
+    fill("white");
+    noStroke();
+    ellipse(320, 320, 140);
+    pop();
 
 }
