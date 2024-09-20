06cf374aa5c0112ee8b04d43a1e67c5ae9deead9
juliax28 <juliax28@outlook.com>
2024-09-20 19:29:22 -0400

    Created the hole in a piece of cheese with hardcoded numbers

--- topics/instructions/variables/CreatingVariables/js/script.js
+++ topics/instructions/variables/CreatingVariables/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Creating Variables
+ * Julia Axiuk
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
@@ -12,13 +12,21 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
+    createCanvas(480, 480);
 
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * hole in piece of cheese
 */
 function draw() {
-
+    //the cheese
+    background(255, 255, 0);
+    //the hole
+    push();
+    noStroke();
+    fill(0);
+    ellipse(140, 175, 100);
+    pop();
 }
