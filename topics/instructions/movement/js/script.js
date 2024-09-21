719fa03ec39a72e7dddc4dce3f48d62efe32322b
juliax28 <juliax28@outlook.com>
2024-09-20 20:40:17 -0400

    Bird exercise without the veolicty portion (the canvas wont appear for some reason)

--- topics/instructions/movement/js/script.js
+++ topics/instructions/movement/js/script.js
@@ -8,17 +8,32 @@
 
 "use strict";
 
+let bird = {
+    x: 120.
+    y: 180,
+    size: 50
+};
+
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
-}
+    createCanvas(640, 480);
+};
 
 
 /**
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-
-}
+    background(0);
+    //move the bird
+    bird.x = bird.x + 1;
+    bird.y = bird.y - 2;
+    //draw the bird
+    push();
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(bird.x, bird.y, bird.size)
+    pop();
+};
