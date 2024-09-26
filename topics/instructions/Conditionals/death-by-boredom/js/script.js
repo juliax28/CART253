5aa8461829eabb541af6efe5a2e876336b3bdc95
juliax28 <juliax28@outlook.com>
2024-09-25 22:31:41 -0400

    Creature now dies of boredom and stays dead

--- topics/instructions/Conditionals/death-by-boredom/js/script.js
+++ topics/instructions/Conditionals/death-by-boredom/js/script.js
@@ -25,7 +25,13 @@ const creature = {
         happy: "#33cc33", // Green
         angry: "#cc3333", // Red
         dead: "#777777" // Grey
-    }
+    },
+    //is the creature alive?
+    alive: true,
+    //how bored is the creature?
+    boredomLevel: 0,
+    //death boredpm threshold
+    deathByBoredom: 500
 };
 
 /**
@@ -47,12 +53,13 @@ function draw() {
 
 
 /**
- * Creature is happy if being massaged and otherwise bored
- */
-/**
+ 
  * Creature is happy if being massaged and otherwise bored
  */
 function checkInput() {
+    if (!creature.alive) {
+        return;
+    }
     // Calculate the distance between the cursor and the creature
     // and put it into a "distance" variable (using const again since
     // we won't change this again later!)
@@ -73,6 +80,12 @@ function checkInput() {
     else {
         // Otherwise the creature is bored
         creature.fill = creature.fills.bored;
+        creature.boredomLevel += 1;
+        if (creature.boredomLevel > creature.deathByBoredom) {
+            //creature dies of boredom
+            creature.alive = false;
+            creature.fill = creature.fills.dead;
+        }
     }
 }
 
