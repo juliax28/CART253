c798681ee5cdb2f489629740e9a7bce2068183a2
juliax28 <juliax28@outlook.com>
2024-09-26 14:23:02 -0700

    Sheep that follows the mouse created

--- topics/CART253_ArtJam1/template-p5-project/js/script.js
+++ topics/CART253_ArtJam1/template-p5-project/js/script.js
@@ -1,24 +1,61 @@
 /**
- * Title of Project
- * Author Name
+ * Crying Wolf
+ * Julia Axiuk
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Click too many times and the sheep will no longer trust you, risking the chance of being eaten
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
+//sheep attributes
+let sheep = 
+{
+    shape:
+    {
+        x: undefined,
+        y: undefined, 
+        size: 50
+    },
+    fills:
+    {
+        alive: "#FFF",
+        dead: "#FF4433"
+    },
 
 }
+//timer
+//let timer = 0;
 
+//wolf attributes
+//TBD
+       
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * canvas creation
 */
-function draw() {
-
+function setup() 
+{
+    createCanvas (400, 400);
 }
+//draw background
+function draw(){
+    background("#B6E853");
+    moveSheep();
+    drawSheep();
+}
+//sheep follows mouse
+function moveSheep(){
+    sheep.shape.x = mouseX;
+    sheep.shape.y = mouseY;
+}
+
+    //draw the sheep
+function drawSheep(){
+    push();
+    fill(sheep.fills.alive);
+    noStroke();
+    ellipse(sheep.shape.x + 15, sheep.shape.y + 15, sheep.shape.size);
+    pop();
+    console.log(sheep.shape.x, sheep.shape.y, sheep.shape.size);
+}
+
