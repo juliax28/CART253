24b81088ecbeb656854fdd618766d940b620e009
juliax28 <juliax28@outlook.com>
2024-09-26 11:58:10 -0700

    messed up circle master

--- topics/instructions/Circle master/js/script.js
+++ topics/instructions/Circle master/js/script.js
@@ -91,8 +91,647 @@ const puck = {
   }
   
   function MovePuck
-  {
-    const d = dist(user.x, user.y, puck.x, puck.y);
+  (
+    const 
+
+    const puck = {
+      x: 350,
+      y: 350,
+      size: 100,
+      fill: "#ff0000"
+    };
+    
+    const user = {
+      x: undefined, // will be mouseX
+      y: undefined, // will be mouseY
+      size: 75,
+      fill: "#000000"
+    };
+    
+    /**
+     * Create the canvas
+     */
+    function setup() {
+      createCanvas(400, 400);
+    }
+    
+    /**
+     * Move the user circle, check for overlap, draw the two circles
+     */
+    function draw() {
+      background("#aaaaaa");
+      
+      // Move user circle
+      moveUser();
+      movePuck();
+      
+      // Draw the user and puck
+      drawUser();
+      drawPuck();
+    }
+    
+    /**
+     * Sets the user position to the mouse position
+     */
+    function moveUser() {
+      user.x = mouseX;
+      user.y = mouseY;
+    }
+    
+    /**
+     * Displays the user circle
+     */
+    function drawUser() {
+      push();
+      noStroke();
+      fill(user.fill);
+      ellipse(user.x, user.y, user.size);
+      pop();
+    }
+    
+    /**
+     * Displays the puck circle
+     */
+    function drawPuck() {
+      push();
+      noStroke();
+      fill(puck.fill);
+      ellipse(puck.x, puck.y, puck.size);
+      pop();
+    }
+    
+    function MovePuck
+    {
+      
+    }
+
+    const puck = {
+      x: 350,
+      y: 350,
+      size: 100,
+      fill: "#ff0000"
+    };
+    
+    const user = {
+      x: undefined, // will be mouseX
+      y: undefined, // will be mouseY
+      size: 75,
+      fill: "#000000"
+    };
+    
+    /**
+     * Create the canvas
+     */
+    function setup() {
+      createCanvas(400, 400);
+    }
+    
+    /**
+     * Move the user circle, check for overlap, draw the two circles
+     */
+    function draw() {
+      background("#aaaaaa");
+      
+      // Move user circle
+      moveUser();
+      movePuck();
+      
+      // Draw the user and puck
+      drawUser();
+      drawPuck();
+    }
+    
+    /**
+     * Sets the user position to the mouse position
+     */
+    function moveUser() {
+      user.x = mouseX;
+      user.y = mouseY;
+    }
+    
+    /**
+     * Displays the user circle
+     */
+    function drawUser() {
+      push();
+      noStroke();
+      fill(user.fill);
+      ellipse(user.x, user.y, user.size);
+      pop();
+    }
+    
+    /**
+     * Displays the puck circle
+     */
+    function drawPuck() {
+      push();
+      noStroke();
+      fill(puck.fill);
+      ellipse(puck.x, puck.y, puck.size);
+      pop();
+    }
+    
+    function MovePuck
+    {
+      
+    }
+
+    const puck = {
+      x: 350,
+      y: 350,
+      size: 100,
+      fill: "#ff0000"
+    };
+    
+    const user = {
+      x: undefined, // will be mouseX
+      y: undefined, // will be mouseY
+      size: 75,
+      fill: "#000000"
+    };
+    
+    /**
+     * Create the canvas
+     */
+    function setup() {
+      createCanvas(400, 400);
+    }
+    
+    /**
+     * Move the user circle, check for overlap, draw the two circles
+     */
+    function draw() {
+      background("#aaaaaa");
+      
+      // Move user circle
+      moveUser();
+      movePuck();
+      
+      // Draw the user and puck
+      drawUser();
+      drawPuck();
+    }
+    
+    /**
+     * Sets the user position to the mouse position
+     */
+    function moveUser() {
+      user.x = mouseX;
+      user.y = mouseY;
+    }
+    
+    /**
+     * Displays the user circle
+     */
+    function drawUser() {
+      push();
+      noStroke();
+      fill(user.fill);
+      ellipse(user.x, user.y, user.size);
+      pop();
+    }
+    
+    /**
+     * Displays the puck circle
+     */
+    function drawPuck() {
+      push();
+      noStroke();
+      fill(puck.fill);
+      ellipse(puck.x, puck.y, puck.size);
+      pop();
+    }
+    
+    function MovePuck
+    {
+      
+    }
+
+    const puck = {
+      x: 350,
+      y: 350,
+      size: 100,
+      fill: "#ff0000"
+    };
+    
+    const user = {
+      x: undefined, // will be mouseX
+      y: undefined, // will be mouseY
+      size: 75,
+      fill: "#000000"
+    };
+    
+    /**
+     * Create the canvas
+     */
+    function setup() {
+      createCanvas(400, 400);
+    }
+    
+    /**
+     * Move the user circle, check for overlap, draw the two circles
+     */
+    function draw() {
+      background("#aaaaaa");
+      
+      // Move user circle
+      moveUser();
+      movePuck();
+      
+      // Draw the user and puck
+      drawUser();
+      drawPuck();
+    }
+    
+    /**
+     * Sets the user position to the mouse position
+     */
+    function moveUser() {
+      user.x = mouseX;
+      user.y = mouseY;
+    }
+    
+    /**
+     * Displays the user circle
+     */
+    function drawUser() {
+      push();
+      noStroke();
+      fill(user.fill);
+      ellipse(user.x, user.y, user.size);
+      pop();
+    }
+    
+    /**
+     * Displays the puck circle
+     */
+    function drawPuck() {
+      push();
+      noStroke();
+      fill(puck.fill);
+      ellipse(puck.x, puck.y, puck.size);
+      pop();
+    }
+    
+    function MovePuck
+    {
+      
+    }
+
+    const puck = {
+      x: 350,
+      y: 350,
+      size: 100,
+      fill: "#ff0000"
+    };
+    
+    const user = {
+      x: undefined, // will be mouseX
+      y: undefined, // will be mouseY
+      size: 75,
+      fill: "#000000"
+    };
+    
+    /**
+     * Create the canvas
+     */
+    function setup() {
+      createCanvas(400, 400);
+    }
+    
+    /**
+     * Move the user circle, check for overlap, draw the two circles
+     */
+    function draw() {
+      background("#aaaaaa");
+      
+      // Move user circle
+      moveUser();
+      movePuck();
+      
+      // Draw the user and puck
+      drawUser();
+      drawPuck();
+    }
+    
+    /**
+     * Sets the user position to the mouse position
+     */
+    function moveUser() {
+      user.x = mouseX;
+      user.y = mouseY;
+    }
+    
+    /**
+     * Displays the user circle
+     */
+    function drawUser() {
+      push();
+      noStroke();
+      fill(user.fill);
+      ellipse(user.x, user.y, user.size);
+      pop();
+    }
+    
+    /**
+     * Displays the puck circle
+     */
+    function drawPuck() {
+      push();
+      noStroke();
+      fill(puck.fill);
+      ellipse(puck.x, puck.y, puck.size);
+      pop();
+    }
+    
+    function MovePuck
+    {
+      
+    }
+
+    const puck = {
+      x: 350,
+      y: 350,
+      size: 100,
+      fill: "#ff0000"
+    };
+    
+    const user = {
+      x: undefined, // will be mouseX
+      y: undefined, // will be mouseY
+      size: 75,
+      fill: "#000000"
+    };
+    
+    /**
+     * Create the canvas
+     */
+    function setup() {
+      createCanvas(400, 400);
+    }
+    
+    /**
+     * Move the user circle, check for overlap, draw the two circles
+     */
+    function draw() {
+      background("#aaaaaa");
+      
+      // Move user circle
+      moveUser();
+      movePuck();
+      
+      // Draw the user and puck
+      drawUser();
+      drawPuck();
+    }
+    
+    /**
+     * Sets the user position to the mouse position
+     */
+    function moveUser() {
+      user.x = mouseX;
+      user.y = mouseY;
+    }
+    
+    /**
+     * Displays the user circle
+     */
+    function drawUser() {
+      push();
+      noStroke();
+      fill(user.fill);
+      ellipse(user.x, user.y, user.size);
+      pop();
+    }
+    
+    /**
+     * Displays the puck circle
+     */
+    function drawPuck() {
+      push();
+      noStroke();
+      fill(puck.fill);
+      ellipse(puck.x, puck.y, puck.size);
+      pop();
+    }
+    
+    function MovePuck
+    {
+      
+    }
+
+    const puck = {
+      x: 350,
+      y: 350,
+      size: 100,
+      fill: "#ff0000"
+    };
+    
+    const user = {
+      x: undefined, // will be mouseX
+      y: undefined, // will be mouseY
+      size: 75,
+      fill: "#000000"
+    };
+    
+    /**
+     * Create the canvas
+     */
+    function setup() {
+      createCanvas(400, 400);
+    }
+    
+    /**
+     * Move the user circle, check for overlap, draw the two circles
+     */
+    function draw() {
+      background("#aaaaaa");
+      
+      // Move user circle
+      moveUser();
+      movePuck();
+      
+      // Draw the user and puck
+      drawUser();
+      drawPuck();
+    }
+    
+    /**
+     * Sets the user position to the mouse position
+     */
+    function moveUser() {
+      user.x = mouseX;
+      user.y = mouseY;
+    }
+    
+    /**
+     * Displays the user circle
+     */
+    function drawUser() {
+      push();
+      noStroke();
+      fill(user.fill);
+      ellipse(user.x, user.y, user.size);
+      pop();
+    }
+    
+    /**
+     * Displays the puck circle
+     */
+    function drawPuck() {
+      push();
+      noStroke();
+      fill(puck.fill);
+      ellipse(puck.x, puck.y, puck.size);
+      pop();
+    }
+    
+    function MovePuck
+    {
+      
+    }
+
+    const puck = {
+      x: 350,
+      y: 350,
+      size: 100,
+      fill: "#ff0000"
+    };
+    
+    const user = {
+      x: undefined, // will be mouseX
+      y: undefined, // will be mouseY
+      size: 75,
+      fill: "#000000"
+    };
+    
+    /**
+     * Create the canvas
+     */
+    function setup() {
+      createCanvas(400, 400);
+    }
+    
+    /**
+     * Move the user circle, check for overlap, draw the two circles
+     */
+    function draw() {
+      background("#aaaaaa");
+      
+      // Move user circle
+      moveUser();
+      movePuck();
+      
+      // Draw the user and puck
+      drawUser();
+      drawPuck();
+    }
+    
+    /**
+     * Sets the user position to the mouse position
+     */
+    function moveUser() {
+      user.x = mouseX;
+      user.y = mouseY;
+    }
+    
+    /**
+     * Displays the user circle
+     */
+    function drawUser() {
+      push();
+      noStroke();
+      fill(user.fill);
+      ellipse(user.x, user.y, user.size);
+      pop();
+    }
+    
+    /**
+     * Displays the puck circle
+     */
+    function drawPuck() {
+      push();
+      noStroke();
+      fill(puck.fill);
+      ellipse(puck.x, puck.y, puck.size);
+      pop();
+    }
+    
+    function MovePuck
+    {
+      
+    }
+
+    const puck = {
+      x: 350,
+      y: 350,
+      size: 100,
+      fill: "#ff0000"
+    };
+    
+    const user = {
+      x: undefined, // will be mouseX
+      y: undefined, // will be mouseY
+      size: 75,
+      fill: "#000000"
+    };
+    
+    /**
+     * Create the canvas
+     */
+    function setup() {
+      createCanvas(400, 400);
+    }
+    
+    /**
+     * Move the user circle, check for overlap, draw the two circles
+     */
+    function draw() {
+      background("#aaaaaa");
+      
+      // Move user circle
+      moveUser();
+      movePuck();
+      
+      // Draw the user and puck
+      drawUser();
+      drawPuck();
+    }
+    
+    /**
+     * Sets the user position to the mouse position
+     */
+    function moveUser() {
+      user.x = mouseX;
+      user.y = mouseY;
+    }
+    
+    /**
+     * Displays the user circle
+     */
+    function drawUser() {
+      push();
+      noStroke();
+      fill(user.fill);
+      ellipse(user.x, user.y, user.size);
+      pop();
+    }
+    
+    /**
+     * Displays the puck circle
+     */
+    function drawPuck() {
+      push();
+      noStroke();
+      fill(puck.fill);
+      ellipse(puck.x, puck.y, puck.size);
+      pop();
+    }
+    
+    function MovePuck
+    {
+      
+    }d = dist(user.x, user.y, puck.x, puck.y);
     const overlap = (d < user.size/ 2 + puck.size / 2);
     if (overlap) {
         const dx = user.x - puck.x;
@@ -109,8 +748,8 @@ const puck = {
         }
         else {
             if (dy < 0) {
-                
+
             }
         }
     }
-  }
+)
