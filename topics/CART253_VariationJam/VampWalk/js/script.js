da2f8c375bcde450a35aa1add168003a440fc72e
juliax28 <juliax28@outlook.com>
2024-11-14 13:45:50 -0800

    canvas created and sprite paths created

--- topics/CART253_VariationJam/VampWalk/js/script.js
+++ topics/CART253_VariationJam/VampWalk/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * VampWalk
+ * Julia Axiuk
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
@@ -8,11 +8,39 @@
 
 "use strict";
 
+
+
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
-function setup() {
 
+
+const vamp = {
+    x: undefined,
+    y:undefined,
+    sprite: undefined,
+    speed: 1,
+    velocity: 4
+    
+    };
+
+const vampSprites = {
+    left:undefined,
+    right:undefined,
+    up:undefined,
+    down:undefined
+}
+
+function preload() {
+    vampSprites.left = loadImage("assets/images/vampLeft.PNG")
+    vampSprites.right = loadImage("assets/images/vampRight.PNG")
+    vampSprites.up = loadImage("assets/images/vampUp.PNG")
+    vampSprites.down = loadImage("assets/images/vampDown.PNG")
+}
+
+
+function setup() {
+createCanvas(580,240);
 }
 
 
@@ -20,5 +48,7 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background("#000000");
+    
 
 }
