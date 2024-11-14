d23f78bba5088f85e268e2e443f673a927f0dafa
juliax28 <juliax28@outlook.com>
2024-11-14 14:14:22 -0800

    got a white circle that moves with the arrow keys, on a black canvas

--- topics/CART253_VariationJam/VampWalk/js/script.js
+++ topics/CART253_VariationJam/VampWalk/js/script.js
@@ -16,8 +16,8 @@
 
 
 const vamp = {
-    x: undefined,
-    y:undefined,
+    x: 200,
+    y:200,
     sprite: undefined,
     speed: 1,
     velocity: 4
@@ -49,6 +49,36 @@ createCanvas(580,240);
 */
 function draw() {
     background("#000000");
-    
+    drawVamp();
+    moveVamp();
 
 }
+
+//draw the vamp
+ function drawVamp(){
+push();
+ellipse(vamp.x,vamp.y, 10)
+fill("#FFFFFF");
+pop();
+ }
+
+ //move the vamp
+
+ function moveVamp(){
+    if (keyIsDown(LEFT_ARROW) === true) {
+        vamp.x -= 1;
+      }
+    
+      if (keyIsDown(RIGHT_ARROW) === true) {
+        vamp.x += 1;
+      }
+    
+      if (keyIsDown(UP_ARROW) === true) {
+        vamp.y -= 1;
+      }
+    
+      if (keyIsDown(DOWN_ARROW) === true) {
+        vamp.y += 1;
+      }
+    
+ }
