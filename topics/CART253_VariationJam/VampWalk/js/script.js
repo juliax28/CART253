241fbe27a4cc534e5fe17985b04d61a241a2aeac
juliax28 <juliax28@outlook.com>
2024-11-26 21:59:28 -0500

    Tried intergrating dialogue box with an array

--- topics/CART253_VariationJam/VampWalk/js/script.js
+++ topics/CART253_VariationJam/VampWalk/js/script.js
@@ -16,6 +16,40 @@
 let state = "title"
 
 
+//Levels Dialogues
+
+
+//Level 01 Dialogue
+
+const level01Dialogue = [
+  "Ahem...",
+  "*cough* *cough* *cough*",
+  "Sorry, sorry... here we go...",
+  "Friends, Romans, countrymen, lend me your ears;",
+  "I come to bury Caesar, not to praise him.",
+  "The evil that men do lives after them;",
+  "The good is oft interred with their bones;",
+  "So let it be with Caesar. The noble Brutus",
+  "Hath told you Caesar was ambitious:",
+  "If it were so, it was a grievous fault,",
+  "And grievously hath Caesar answer’d it.",
+  "*cough* Thank you *cough*"
+];
+
+
+//dialogue index
+
+let dialogueIndex = 0;
+// Dialog box specification
+let speechBox = {
+  x: 50,
+  y: 300,
+  width: 300,
+  height: 80,
+  padding: 20,
+  fontSize: 18
+};
+
 const vampSprites = {
   left: undefined,
   right: undefined,
@@ -46,8 +80,8 @@ let vamp = {
 let path = {
   x: 200,
   y: 200,
-  width: 55,
   height: 55,
+  width: 300,
 };
 
 
@@ -71,10 +105,7 @@ function draw() {
   if (state === "game")
     game();
 
-  if (state === "gameOver") {
-    gameOverHunger();
 
-  }
 }
 
 //draw the vamp
@@ -82,11 +113,7 @@ function title() {
   background("#ad2222");
 }
 
-function mousePressed() {
-  if (state === "title") {
-    state = "game";
-  }
-}
+
 
 
 function game() {
@@ -95,8 +122,11 @@ function game() {
   moveVamp();
   drawVamp();
   checkVampPathOverlap();
-
+  showDialog();
 }
+
+
+
 function drawVamp() {
   push();
   ellipse(vamp.x, vamp.y, vamp.size)
@@ -159,19 +189,51 @@ function resetSprite() {
 }
 function checkVampPathOverlap() {
   // First: is the right side of the user rect to the right or the left side of the target?
-  const vampPathOverlap = (vamp.x + vamp.size / 2 > path.x - path.width / 2 &&
+  const vampPathOverlap = (vamp.x + vamp.size / 2 > path.x - path.height / 2 &&
     // Second: is the left side of the user rect to the left of the right side of the target?
-    vamp.x - vamp.size / 2 < path.x + path.width / 2 &&
+    vamp.x - vamp.size / 2 < path.x + path.height / 2 &&
     // Third: is the bottom of the user rect below the top of the target?
-    vamp.y + vamp.size / 2 > path.y - path.height / 2 &&
+    vamp.y + vamp.size / 2 > path.y - path.width / 2 &&
     // Fourth: is the top of the user rect above the bottom of the target?
-    vamp.y - vamp.size / 2 < path.y + path.height / 2);
+    vamp.y - vamp.size / 2 < path.y + path.width / 2);
   if (!vampPathOverlap) {
 
     vamp.y = vamp.y + vamp.velocity;
-    console.log(vampPathOverlap);
+
   }
 
 }
 
+function showDialog() {
+  // The background box
+  push();
+  fill(0);
+  stroke(255);
+  strokeWeight(3);
+  rect(box.x, box.y, box.width, box.height);
+  pop();
+
+  // The text
+  // Note how we're using extra arguments in text() to specify
+  // the dimensions we want the text to wrap inside, including
+  // using the padding of the dialog box
+  push();
+  fill(255);
+  textSize(18);
+  text(level01Dialogue[dialogueIndex], speechBox.x + speechBox.padding, speechBox.y + speechBox.padding, speechBox.width - 2 * speechBox.padding, speechBox.height - 2 * speechBox.padding);
+  pop();
+}
+
+function mousePressed() {
+  if (state === "title") {
+    state = "game";
+  }
+  if (state === "game") {
+    // Make sure we're not at the end of the speech aready
+    if (dialogueIndex < level01Dialogue.length - 1) {
+      // Advance the line
+      dialogueIndex++;
+    }
+  }
+}
 
